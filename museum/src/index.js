bbabbb5925371942b6d9941c47d43c7e396e5339
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-09-25 02:06:34 +0300

    feat:add Section Visiting. Google Street View

--- museum/src/index.js
+++ museum/src/index.js
@@ -10,6 +10,7 @@ const prev = document.getElementById('btn-prev');
 const next= document.getElementById('btn-next');
 const welcomeSlides = document.querySelectorAll('.slide-img');
 const welcomeDots = document.querySelectorAll('.dots');
+const sliderNumber = document.querySelector('.slider-current')
 let currentSlider = 0;
 
 
@@ -17,11 +18,13 @@ const activeWelcomeSlide = n => {
   console.log(n)
   welcomeSlides.forEach(slide=>slide.classList.remove('active'))
   welcomeSlides[n].classList.add('active')
+  sliderNumber.innerHTML = `0${n+1} | 05`
 }
 const activeWelcomeDots = n => {
   console.log(n)
   welcomeDots.forEach(dot=>dot.classList.remove('active-dot'))
   welcomeDots[n].classList.add('active-dot')
+ 
 }
 
 const chenchActivSlide = i =>{
