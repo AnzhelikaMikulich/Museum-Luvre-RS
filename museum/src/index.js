ab408261894d4198ede1ca31a7fdea48bcbe7a96
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-09-24 05:39:08 +0300

    feat:start task Museum add Welcome slider

--- museum/src/index.js
+++ museum/src/index.js
@@ -1,10 +1,54 @@
-import image from './images/lazy.png';
+const progress = document.querySelectorAll('.progress');
+progress.forEach(element => element.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value}%, #fff ${value}%, white 100%)`
+}))
 
-const subHeader = document.createElement('h2');
-subHeader.innerHTML = 'This elements was created by FS';
+//-------Welcome Slider-------//
 
-const myImage = new Image();
-myImage.src = image;
+const prev = document.getElementById('btn-prev');
+const next= document.getElementById('btn-next');
+const welcomeSlides = document.querySelectorAll('.slide-img');
+const welcomeDots = document.querySelectorAll('.dots');
+let currentSlider = 0;
 
-document.body.appendChild(subHeader);
-document.body.appendChild(myImage);
+
+const activeWelcomeSlide = n => {
+  console.log(n)
+  welcomeSlides.forEach(slide=>slide.classList.remove('active'))
+  welcomeSlides[n].classList.add('active')
+}
+const activeWelcomeDots = n => {
+  console.log(n)
+  welcomeDots.forEach(dot=>dot.classList.remove('active-dot'))
+  welcomeDots[n].classList.add('active-dot')
+}
+
+const chenchActivSlide = i =>{
+  activeWelcomeSlide(i)
+  activeWelcomeDots(i)
+
+}
+
+const nextWelcomeSlide = ()=>{
+  if(currentSlider == welcomeSlides.length-1){
+    currentSlider = 0;
+    chenchActivSlide(currentSlider)
+  }else{
+    currentSlider++;
+    chenchActivSlide(currentSlider)
+  }
+}
+
+const prevWelcomeSlide = ()=>{
+  if(currentSlider == 0){
+    currentSlider = welcomeSlides.length - 1;
+    chenchActivSlide(currentSlider)
+  }else{
+    currentSlider--;
+    chenchActivSlide(currentSlider)
+  }
+}
+
+next.addEventListener('click', nextWelcomeSlide);
+prev.addEventListener('click', prevWelcomeSlide)
