d99cb07d41cca3223bc3ef13f96e7e8bac9e08cb
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-09-28 07:03:27 +0300

    feat:add Tickets form

--- museum/src/index.js
+++ museum/src/index.js
@@ -7,7 +7,9 @@ const progress = document.querySelectorAll(".progress");
 progress.forEach((element) =>
   element.addEventListener("input", function () {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value}%, #fff ${value}%, white 100%)`;
+    this.style.background = `linear-gradient(to right, 
+      #710707 0%, 
+      #710707 ${value}%, #fff ${value}%, white 100%)`;
   })
 );
 
@@ -134,3 +136,30 @@ document.body.addEventListener("touchmove", (e) => {
   worksExploreSlider(x);
   pauseEvents(e);
 });
+
+// ========Ripple=========
+
+const rippleButton = document.querySelector('.ripple')
+
+
+rippleButton.addEventListener('click', function (e) {
+  console.log('ripple')
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+
